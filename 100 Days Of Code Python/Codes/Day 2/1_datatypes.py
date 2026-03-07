c886ae85582899020bda4b2c321def3e82ba272c
amiteshkumarofficial20 <amiteshkumarofficial.20@gmail.com>
2026-03-07 18:04:33 +0530

    Updated Data Types

--- 100 Days Of Code Python/Codes/Day 2/1_datatypes.py
+++ 100 Days Of Code Python/Codes/Day 2/1_datatypes.py
@@ -11,15 +11,16 @@
 print("Hello"[0]) #H
 #we can specify here negative as well as positive indices
 print("Hello"[-1]) #o -1/4
+print("Hello"[0:2]) #Slicing
 
-# String
+# String Concatenation 
 print("123" + "345")
 
 # Integer = Whole number
 print(123 + 345)
 
-# Large Integers
-print(123_456_789)
+# Large Integers we can write as _
+print(123_456_789) #same as 123456789
 
 # Float = Floating Point Number
 print(3.14159)
