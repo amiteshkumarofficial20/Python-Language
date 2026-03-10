b627c5d5096a2290bf35eddd2729cb45442d82d2
amiteshkumarofficial20 <amiteshkumarofficial.20@gmail.com>
2026-03-10 12:35:20 +0530

    Day 3 Updated

--- 100 Days Of Code Python/Codes/Day 3/3_nestedifandelifstatements.py
+++ 100 Days Of Code Python/Codes/Day 3/3_nestedifandelifstatements.py
@@ -29,7 +29,9 @@ if height >= 120:
         print("Please pay $5.")
     elif age <= 18:
         print("Please pay $7.")
-    else:
+    elif age <= 21:
         print("Please pay $12.")
+    else:
+        print("Please pay $20.")
 else:
     print("Sorry you have to grow taller before you can ride.")
